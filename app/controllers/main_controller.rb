404a4362926a7741cfc5671f81c52b2d654aebf9
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-11-17 17:53:17 -0300

    agrego campo username al signup

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     before_action :authenticate_user!    
 def home
+    "holaaaaaaaaaaaaaaaa"
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'registrations'
+  }
   root "main#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
