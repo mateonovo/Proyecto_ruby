585aca78694fdf123e56a558c27d0907d83a178f
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-11-20 20:17:29 -0300

    views users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,17 @@
 class UsersController < ApplicationController
-    #before_action :configure_permitted_parameters, if: :devise_controller?
-
-
-
-
-
-  # GET /users
-    def index
-    end
-
-
-  # GET /users/1
-    def show
-      render json: @user
-    end
-  # POST /users
-    def create
-      @user = User.new(user_params)
-  if @user.save
-        render json: @user, status: :created, location: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-  # PATCH/PUT /users/1
-    def update
-      if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-  # DELETE /users/1
-    def destroy
-      @user.destroy
-    end
-  private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_user
-        @user = User.find(params[:id])
-      end
-  # Only allow a trusted parameter "white list" through.
-      def user_params
-        params.require(:user).permit(:name, :username, :email)
-      end
+  def index
   end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def show
+    @user = current_user
+  end
+end
