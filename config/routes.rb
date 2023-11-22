23c5ab60f906238a3ad60b306eac5adc54f2597f
NOVOMISKI MATEO MAXIMO <mateonovo1@gmail.com>
2023-11-22 16:44:30 -0300

    agrego links , arreglo home  , borrar usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :usuarios
+  resources :links
   devise_for :users, :controllers => {
     registrations: 'registrations'
   }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_133816) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_192342) do
   create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.string "url"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,9 +29,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_133816) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username" 
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "links", "users"
 end
